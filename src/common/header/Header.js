bad3997e25e9da8675124674d2127f2240cd7296
Roshni <roshni.cse.01@gmail.com>
2021-05-09 19:53:27 +0530

    Checkpoint 2:Creating the Header Component

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 import MovieLogo from "../../assets/logo.svg";
 import "./Header.css";
 import Button from "@material-ui/core/Button";
-
-
+import { Link } from "react-router-dom";
 class Header extends Component {
   render() {
-    let token;
+    console.log(this.props)
     return (
       <div>
-        <header class="header">
-          <img src={MovieLogo} alt="logo" id="image" />
-          
-        </header>
-        
-       
+      <header className="header">
+        <img src={MovieLogo} alt="logo" id="image" />
+        <div className="login-button">
+          <Button
+            variant="contained"
+            color="default"
+            onClick={this.openModalHandler}
+          >
+            Login
+          </Button>
+        </div>
+        {this.props.showBookShowButton ?
+        <div className="booknow-button">
+            <Link to={"/bookshow/" + this.props.id}>
+              <Button variant="contained" color="primary">
+                Book Show
+              </Button>
+            </Link>
+            </div>
+          : ""}
+      </header>
       </div>
     );
   }
--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -4,21 +4,19 @@ import Details from './details/Details';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BookShow from '../screens/bookshow/BookShow';
 import Confirmation from '../screens/confirmation/Confirmation';
+import moviesData from '../assets/moviesData';
 
 class Controller extends Component {
 
-  constructor() {
-    super();
-    this.baseUrl = "http://3.227.145.17:8085/api/v1/";
-  }
+  
   render() {
     return (
       <Router>
         <div className="main-container">
-        <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
-          <Route path='/movie/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
-          <Route path='/bookshow/:id' render={(props) => <BookShow {...props} baseUrl={this.baseUrl} />} />
-          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} baseUrl={this.baseUrl} />} />
+        <Route exact path='/' render={(props) => <Home {...props} movieData={moviesData} />} />
+          <Route path='/movie/:id' render={(props) => <Details {...props}  />} />
+          <Route path='/bookshow/:id' render={(props) => <BookShow {...props}  />} />
+          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} />} />
         </div>
       </Router>
     )
